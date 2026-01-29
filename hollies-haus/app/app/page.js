b84f7e60c58336c9a27e7389324f2ea6dcff86af
guaped <63273936+guaped@users.noreply.github.com>
2026-01-28 20:05:29 -0500

    Delete hollies-haus/app/app directory

--- hollies-haus/app/app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Hollies Haus</h1>
-      <p>Pre-owned CDs</p>
-      <p>We are live 💿</p>
-    </main>
-  );
-}
